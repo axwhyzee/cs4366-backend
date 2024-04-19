d15a09da1b383d816b8d89f9f20d1f057e105d41
Siah Wee Hung <siah_weehung@yahoo.com>
2024-04-19 15:31:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import requests
 import cv2
 
 
-r = requests.post('http://127.0.0.1:5000/id_from_img/', json={'img': cv2.imread('obj_detect_backend/photo_2024-04-19 13.34.02.jpeg').tolist()})
+API_URL = 'http://127.0.0.1:5000'
+r = requests.post(f'{API_URL}/id_from_img/', json={'img': cv2.imread('obj_detect_backend/photo_2024-04-19 13.34.02.jpeg').tolist()})
 print(r.text)
